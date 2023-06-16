4815cf3c77eeecab0c9bdf499d377ea2a7a83a36
Vishant Kumar <vishant750@gmail.com>
2023-06-17 02:35:59 +0530

    flash message added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,flash
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
 from wtforms.validators import DataRequired
@@ -34,6 +34,7 @@ def name():
     if form.validate_on_submit():
         name=form.name.data
         form.name.data=''
+        flash("Form submitted successfully")
     return render_template('name.html',name=name,form=form)
 
 
